350a1ca5ec7592ce08a08629b3714e2dfb211ef8
makela <johnhopeful11@gmail.com>
2020-10-26 17:15:19 +0200

    dorabotali eto govno

--- calc.py
+++ calc.py
@@ -1,20 +1,22 @@
 def correct_check(exp):
+    sign_first = exp[0] == '+' or exp[0] == '-' or exp[0] == '*' or exp[0] == '/'
+    sign_last = exp[-1] == '+' or exp[-1] == '-' or exp[-1] == '*' or exp[-1] == '/'
     for i in exp:
         if i.isalpha():
-            return(print('В выражении не могут быть буквы'))
+            return 'В выражении не могут быть буквы'
 
-    if exp[0] == '+' or exp[0] == '-' or exp[0] == '*' or exp[0] == '/':
-        return(print('Выражение не может начинаться со знака'))
+    if sign_first:
+        return 'Выражение не может начинаться со знака'
 
-    elif exp[-1] == '+' or exp[-1] == '-' or exp[-1] == '*' or exp[-1] == '/':
-        return(print('Выражение не может заканчиваться на знак'))
+    elif sign_last:
+        return 'Выражение не может заканчиваться на знак'
 
     elif len(exp) < 3:
-        return(print('Выражение слишком короткое'))
+        return 'Выражение слишком короткое'
 
     elif '+' not in exp and '-' not in exp and '*' not in exp and '/' not in exp:
-        return(print('В выражении нет знаков'))
-        
+        return 'В выражении нет знаков'
+
     else:
         return exp
 
@@ -22,57 +24,25 @@ def correct_check(exp):
 def splitter(exp):
     if '+' in correct_check(exp):
         spl = exp.split('+')
-        spl = [int(i) for i in spl if i != '+']
-        spl.append('+')
-        return spl
-
+        return int(spl[0]) + int(spl[1])
     elif '-' in correct_check(exp):
         spl = exp.split('-')
-        spl = [int(i) for i in spl if i != '-']
-        spl.append('-')
-        return spl
-
+        return int(spl[0]) - int(spl[1])
     elif '*' in correct_check(exp):
         spl = exp.split('*')
-        spl = [int(i) for i in spl if i != '*']
-        spl.append('*')
-        return spl
-
+        return int(spl[0]) * int(spl[1])
     elif '/' in correct_check(exp):
         spl = exp.split('/')
-        spl = [int(i) for i in spl if i != '/']
-        spl.append('/')
-        return spl
-      
-
-    return spl
+        return int(spl[0]) / int(spl[1]) if int(spl[1]) != 0 else 'На ноль делить нельзя'
 
 
-def plus(exp):
-    if splitter(exp)[2] == '+':
-        return splitter(exp)[0] + splitter(exp)[1]
+def retry(exp):
+    while exp != correct_check(exp):
+        print(correct_check(exp))
+        exp = input('Введите выражение заново: ')
+    return splitter(exp)
 
 
-def minus(exp):
-    if splitter(exp)[2] == '-':
-        return splitter(exp)[0] - splitter(exp)[1]
-
-
-def times(exp):
-    if splitter(exp)[2] == '*':
-        return splitter(exp)[0] * splitter(exp)[1]
-
-
-def divide(exp):
-    if splitter(exp)[1] == 0:
-        return print('На ноль делить нельзя')
-    elif splitter(exp)[2] == '/':
-        return splitter(exp)[0] / splitter(exp)[1]        
-
-
-
-
-
-#вызов главной функии 
-exp = input('Введите выражение: ')
-print(divide(exp))
+if __name__ == '__main__':
+    exp = input('Введите выражение: ')
+    print(retry(exp))
